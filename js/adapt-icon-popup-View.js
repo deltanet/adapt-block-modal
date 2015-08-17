799efbf37344b11e53ee0ea262ca17aa6d7e5b03
RobertPeek <robert@delta-net.co.uk>
2015-08-17 09:58:09 +0100

    Updated for framework v2.0.1

--- js/adapt-icon-popup-View.js
+++ js/adapt-icon-popup-View.js
@@ -20,9 +20,7 @@ define(function(require) {
 
         events: {
             "click .icon-popup-graphic-button":"openPopup",
-            "click .icon-popup-open-button":"openPopup",
-            "click .content-popup-icon-close":"closeContent",
-            "click .icon-popup-shadow":"closeContent",
+            "click .icon-popup-open-button":"openPopup"
         },
 
         preRender: function() {
@@ -33,75 +31,30 @@ define(function(require) {
             var data = this.model.toJSON();
             var template = Handlebars.templates["icon-popup"];
 
-            var extLoc = this.model.get('_iconPopup')._location;
-            // Push data into template and append template
-            if(extLoc=="article"){
-                $(this.el).html(template(data)).appendTo('.' + this.model.get("_id") + " > .article-inner ");
-            }
-            if(extLoc=="block"){
-                $(this.el).html(template(data)).appendTo('.' + this.model.get("_id") + " > .block-inner ");
-            }
-            if(extLoc=="component"){
-                $(this.el).html(template(data)).appendTo('.' + this.model.get("_id") + " > .component-inner ");
-            }
+            $(this.el).html(template(data)).prependTo('.' + this.model.get("_id") + " > ." + this.model.get("_type")+"-inner");
         },
         
         openPopup: function(event) {
 
             if (event) event.preventDefault();
-            Adapt.trigger('popup:opened');
-            this.$('.content-popup-icon-close').attr('tabindex', 0);
 
             var $item = $(event.currentTarget);
-            var index = $item.index();
-            console.log(index);
+            var currentItem = this.getCurrentItem($item.index());
+            var popupObject = {
+                title: currentItem.title,
+                body: "<div class='icon-popup-notify-body'>" +
+                currentItem.body + "</div><img class='icon-popup-notify-graphic' src='"+
+                currentItem._itemGraphic.src +"' alt='"+
+                currentItem._itemGraphic.alt +"'/>"
+            };
+
+            Adapt.trigger("notify:popup", popupObject);
             $item.addClass("visited");
-            this.showContentWithItemIndex(index);
-            $(".content-popup-icon-close").focus();
+
         },
 
-        showContentWithItemIndex: function(index) {
-            this.$(".icon-popup-content-item").css({
-                display:"none"
-            });
-            
-            this.$(".icon-popup-content-item").eq(index).css({
-                display:"block"
-            });
-
-            var $content = this.$(".icon-popup-content");
-            $content.css({ 
-                marginTop: -($content.height() / 2) + "px"
-            }).velocity({
-                opacity: 1,
-                translateY: 0
-            },{
-                display: "block"
-            });
-
-            this.$(".icon-popup-shadow").velocity({
-                opacity: 1
-            },{
-                display: "block"
-            });
-        },
-
-        closeContent: function(event) {
-            if (event) event.preventDefault();
-            this.$(".icon-popup-content").velocity({
-                opacity: 0,
-                translateY: "-50px"
-            },{
-                display: "none"
-            });
-
-            Adapt.trigger('popup:closed');
-
-            this.$(".icon-popup-shadow").velocity({
-                opacity: 0
-            },{
-                display: "none"
-            });
+        getCurrentItem: function(index) {
+            return this.model.get('_iconPopup')._items[index];
         }
 
     });
