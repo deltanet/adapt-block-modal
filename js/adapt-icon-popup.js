f98e65d893abf3fcf51db5a6beaf185d9190acc5
RobertPeek <robert@delta-net.co.uk>
2015-10-28 12:58:30 +0000

    Added Accessibility and RTL support

--- js/adapt-icon-popup.js
+++ js/adapt-icon-popup.js
@@ -8,11 +8,63 @@ define(function(require) {
 
     var Adapt = require('coreJS/adapt');
     var Backbone = require('backbone');
-    var IconPopupView = require('extensions/adapt-icon-popup/js/adapt-icon-popup-View');
+    var IconPopup = Backbone.View.extend({
 
-    // Listen to when the data is all loaded
-    Adapt.on('app:dataReady', function() {
-        //console.log('adapt-icon-popup extension has loaded and data is ready');
+        className: "extension-icon-popup",
+
+        initialize: function () {
+            this.listenTo(Adapt, 'remove', this.remove);
+            this.preRender();
+            this.render();
+        },
+
+        events: {
+            "click .icon-popup-graphic-button":"openPopup",
+            "click .icon-popup-open-button":"openPopup"
+        },
+
+        preRender: function() {},
+
+        render: function () {
+
+            var data = this.model.toJSON();
+            var template = Handlebars.templates["icon-popup"];
+
+            $(this.el).html(template(data)).prependTo('.' + this.model.get("_id") + " > ."+this.model.get("_type")+"-inner");
+
+            this.$('.icon-popup-inner').addClass(this.model.get("_type"));
+
+        },
+        
+        openPopup: function(event) {
+
+            if (event) event.preventDefault();
+
+            var $item = $(event.currentTarget);
+            var currentItem = this.getCurrentItem($item.index());
+            var popupObject = {
+                title: currentItem.title,
+                body: "<div class='icon-popup-notify-body'>" +
+                currentItem.body + "</div><img class='icon-popup-notify-graphic' src='"+
+                currentItem._itemGraphic.src +"' alt='"+
+                currentItem._itemGraphic.alt +"'/>"
+            };
+
+            Adapt.trigger("notify:popup", popupObject);
+            $item.addClass("visited");
+
+        },
+
+        getCurrentItem: function(index) {
+            return this.model.get('_iconPopup')._items[index];
+        }
+
+    });
+    
+    Adapt.on('articleView:postRender blockView:postRender componentView:postRender', function(view) {
+        if (view.model.get("_iconPopup")) {
+          new IconPopup({model:view.model});
+        }
     });
 
 });
